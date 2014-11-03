f0a661e3f67eace719c8674533002795d1a5e0b0
Carlos Aguilar <agustin3a94@gmail.com>
2014-11-03 08:48:37 -0600

    Initial commit

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,49 @@
+class MapsController < ApplicationController
+  def map
+  	@places = Place.all
+		@geojson = Array.new
+		@places.each do |place|
+			if place.type == 1 
+				color = '#E74C3C'
+				symbol = 'park'
+			elsif place.type == 2
+				color = '#1abc9c'
+				symbol = 'place-of-worship'
+			elsif place.type == 3
+				color = '#27ae60'
+				symbol = 'museum'
+			elsif place.type == 4
+				color = '#e67e22'
+				symbol = 'building'
+			elsif place.type == 5
+				color = '#3498db'
+				symbol = 'restaurant'
+			elsif place.type == 6
+				color = '#f1c40f'
+				symbol = 'shop'
+			end
+		ptype = place.type.to_s
+		  @geojson << {
+		    type: 'Feature',
+		    geometry: {
+		      type: 'Point',
+		      coordinates: [place.longitude, place.latitude]
+		    },
+		    properties: {
+		      name: place.name,
+		      address: place.address,
+		      phone: place.phone,
+		      website: place.website,
+		      type: ptype,
+	      	:'marker-color' => color,
+	      	:'marker-symbol' => symbol,
+	      	:'marker-size' => 'medium'		      
+		    }
+		  }
+		end
+		respond_to do |format|
+  	format.html
+  	format.json { render json: @geojson }  # respond with the created JSON object
+end
+  end
+end
---
+++ app/models/place.rb
@@ -0,0 +1,3 @@
+class Place < ActiveRecord::Base
+	self.inheritance_column = nil
+end
